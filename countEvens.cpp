ae087dbcd7f3d4d75e6947e06fcac6c0f4afc93a
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2022-10-19 19:43:11 -0700

    remove content in countEven function

--- countEvens.cpp
+++ countEvens.cpp
@@ -3,11 +3,5 @@
 #include <iostream>
 
 int countEvens(int a[], int size) {
-    int numEvens = 0;
-    for(int i =0; i< size; i++){
-        if(a[i]%2 ==0){
-            numEvens++;
-        }
-    }
-  return numEvens; // STUB!  Replace with correct code.
+  return -42; // STUB!  Replace with correct code.
 }
